9b86fc235a83222a2393525521b76686b628bb0f
rustaceanrob <rob.netzke@gmail.com>
2026-09-15 15:42:03 +0100

    Add Socks5 proxy connection type

--- src/socks5.rs
+++ src/socks5.rs
@@ -1,4 +1,20 @@
+use std::{
+    io::{Read, Write},
+    net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, TcpStream},
+    time::Duration,
+};
+
 use bitcoin_hashes::sha3_256;
+
+const VERSION: u8 = 5;
+const NOAUTH: u8 = 0;
+const METHODS: u8 = 1;
+const CMD_CONNECT: u8 = 1;
+const RESPONSE_SUCCESS: u8 = 0;
+const RSV: u8 = 0;
+const ADDR_TYPE_IPV4: u8 = 1;
+const ADDR_TYPE_DOMAIN: u8 = 3;
+const ADDR_TYPE_IPV6: u8 = 4;
 
 const SALT: &[u8] = b".onion checksum";
 const TOR_VERSION: u8 = 0x03;
@@ -18,6 +34,105 @@ impl core::fmt::Display for OnionAddress {
         let buf = pubkey_to_service(&self.0);
         // Every byte comes from ALPHABET (ASCII) or the literal b".onion".
         f.write_str(std::str::from_utf8(&buf).expect("ASCII"))
+    }
+}
+
+pub trait SocksDestination {
+    type Encoded: AsRef<[u8]>;
+    const TYPE_BYTE: u8;
+    fn encode(&self) -> Self::Encoded;
+}
+
+impl SocksDestination for OnionAddress {
+    type Encoded = [u8; 63];
+    const TYPE_BYTE: u8 = ADDR_TYPE_DOMAIN;
+
+    fn encode(&self) -> [u8; 63] {
+        let service = pubkey_to_service(&self.0);
+        let mut out = [0u8; 63];
+        out[0] = 62;
+        out[1..].copy_from_slice(&service);
+        out
+    }
+}
+
+impl SocksDestination for Ipv4Addr {
+    type Encoded = [u8; 4];
+    const TYPE_BYTE: u8 = ADDR_TYPE_IPV4;
+
+    fn encode(&self) -> [u8; 4] {
+        self.octets()
+    }
+}
+
+impl SocksDestination for Ipv6Addr {
+    type Encoded = [u8; 16];
+    const TYPE_BYTE: u8 = ADDR_TYPE_IPV6;
+
+    fn encode(&self) -> [u8; 16] {
+        self.octets()
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Socks5Proxy {
+    proxy: SocketAddr,
+    timeout: Duration,
+}
+
+impl Socks5Proxy {
+    pub const DEFAULT_TOR_PROXY: Self = Self {
+        proxy: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 9050),
+        timeout: Duration::from_secs(1),
+    };
+
+    pub const fn from_proxy_socket_addr(proxy: SocketAddr, timeout: Duration) -> Self {
+        Self { proxy, timeout }
+    }
+
+    pub fn connect<D: SocksDestination>(&self, addr: D, port: u16) -> std::io::Result<TcpStream> {
+        let mut tcp_stream = TcpStream::connect_timeout(&self.proxy, self.timeout)?;
+        tcp_stream.write_all(&[VERSION, METHODS, NOAUTH])?;
+        let mut resp_buf = [0_u8; 2];
+        tcp_stream.read_exact(&mut resp_buf)?;
+        if resp_buf[0] != VERSION {
+            return Err(std::io::Error::other("unsupported socks protocol version"));
+        }
+        if resp_buf[1] != NOAUTH {
+            return Err(std::io::Error::other("socks proxy requires authentication"));
+        }
+        let dest_bytes = addr.encode();
+        tcp_stream.write_all(&[VERSION, CMD_CONNECT, RSV, D::TYPE_BYTE])?;
+        tcp_stream.write_all(dest_bytes.as_ref())?;
+        tcp_stream.write_all(&port.to_be_bytes())?;
+        let mut resp_buf = [0_u8; 4];
+        tcp_stream.read_exact(&mut resp_buf)?;
+        if resp_buf[0] != VERSION {
+            return Err(std::io::Error::other("unsupported socks protocol version"));
+        }
+        if resp_buf[1] != RESPONSE_SUCCESS {
+            return Err(std::io::Error::other("response failure"));
+        }
+        match resp_buf[3] {
+            ADDR_TYPE_IPV4 => {
+                // Read the IPv4 address and additional two bytes for the port
+                let mut buf = [0_u8; 6];
+                tcp_stream.read_exact(&mut buf)?;
+            }
+            ADDR_TYPE_IPV6 => {
+                // Read the IPv6 address and additional two bytes for the port
+                let mut buf = [0_u8; 18];
+                tcp_stream.read_exact(&mut buf)?;
+            }
+            ADDR_TYPE_DOMAIN => {
+                let mut len = [0_u8; 1];
+                tcp_stream.read_exact(&mut len)?;
+                let mut buf = vec![0_u8; u8::from_le_bytes(len) as usize];
+                tcp_stream.read_exact(&mut buf)?;
+            }
+            _ => return Err(std::io::Error::other("response failure")),
+        }
+        Ok(tcp_stream)
     }
 }
 
